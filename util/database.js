cf47acafaeb65f0dfd85062b51907f980dc331b8
Rahim <k.rahim001@gmail.com>
2023-08-24 18:18:54 +0530

    RJ(db-connection-and-product-models): added

--- util/database.js
+++ util/database.js
@@ -1,10 +1,21 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: 'void main',
+// });
+
+// module.exports = pool.promise();
+
+// sequelize connect
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'void main', {
+    dialect: 'mysql',
     host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'void main',
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
